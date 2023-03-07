33417f32bf0514c636564608c14403aa4f161cdc
Goncalves <goncalca@schaeffler.com>
2023-03-07 13:17:14 -0300

    updating

--- Frontend/src/components/ExchangeArchives/ExchangeArchives.js
+++ Frontend/src/components/ExchangeArchives/ExchangeArchives.js
@@ -1,119 +1,27 @@
-import axios from 'axios';
 import React, {useEffect, useState} from 'react';
-import ODataStore from "devextreme/data/odata/store";
-//import './ExchangeArchives.css'
+import { json } from 'react-router-dom';
+import axios from 'axios';
+//DataGrid Definition 
+function ExchangeArchives () {
 
-import DataGrid, {
-  Column,
-  Grouping,
-  GroupPanel,
-  Pager,
-  Paging,
-  SearchPanel,
-  Editing,
-  FilterRow,
-  ColumnChooser,
-  dataSource
-} from "devextreme-react/data-grid";
-
-const pageSizes = [10, 25, 50, 100];
-
-const APIdata = () => {
-     //API Receive
-    const [posts, setPosts ] = useState([]);
-
-    const getPosts = async () =>{
-        try {
-            
-           const response = await axios.get("https://localhost:7063/api/MvSysSefExchangeFile");
-            
-                const data = response.data;
-                setPosts(data);
-                console.log(data)
-                return(data);
-            
-        } catch (error){
-            console.log("erro em ExchangeArchives");
-            console.log(error);
-        }
-    };
+    const [posts, setPosts] = useState([]);
 
     useEffect(() =>{
-        getPosts();
-    }, []);
+        axios.get("https://localhost:7063/api/MvSysSefExchangeFile")
+        .then(response => response.json())
+        .then(data => setPosts(data))
+        .catch(error => console.log(error))
+    },[])
+ 
 
-    
-}
-
-//DataGrid Definition 
-class ExchangeArchives extends React.Component {
-  constructor(props) {
-    super(props);
-    this.datareceive = APIdata.setPosts;
-    this.state = {
-      collapsed: false,
-    };
-    this.onContentReady = this.onContentReady.bind(this);
-
-   
-  }
-
-render() {
+  console.log(posts);
+ 
   return(
       <div className="ExchangeArchives">
-        <header className="ExchangeArchives-header">
-          <DataGrid
-            dataSource={this.datareceive}
-            keyExpr="sefId"
-            allowColumnReordering={true}
-            rowAlternationEnabled={true}
-            showBorders={true}
-            onContentReady={this.onContentReady}
-          >
-          <Column dataField="sefCompany"/>
-          <Column dataField="sefId"/>
-          <Column dataField="sefOperation"/>
-          <Column dataField="sefSourceHost"/>
-          <Column dataField="sefSourceDir"/>
-          <Column dataField="sefSourceFile"/>
-          <Column dataField="sefDestHost"/>
-          <Column dataField="sefDestDir"/>
-          <Column dataField="sefDestFile"/>
-          <Column dataField="sefBackupDir"/>
-          <Column dataField="sefShare"/>
-          <Column dataField="sefDestShare"/>
-          <Column dataField="sefDeleteSource"/>
-          <Column dataField="sefAppend"/>
-          <Column dataField="sefUserAlt"/>
-          <Column dataField="sefDateAlt"/>
-          <Column dataField="sefSourceFtpUser"/>
-          <Column dataField="sefDestFtpUser"/>
-          <Column dataField="sefModule"/>
-          <Editing
-            mode="popup"
-            allowUpdating={true}
-            allowDeleting={true}
-            allowAdding={true}
-          />
-          <Pager allowedPageSizes={pageSizes} showPageSizeSelector={true} />
-          <Paging defaultPageSize={50} />
-          <FilterRow visible={true} />
-          <SearchPanel visible={true} />
-          <ColumnChooser enabled={true} />
-        </DataGrid>
-      </header>
-    </div>       
-  );
+      <p>{json.stringify(posts)}</p>
+      </div>       
+  )
+  
 }
- onContentReady(e) {
-    if (!this.state.collapsed) {
-      e.component.expandRow(["EnviroCare"]);
-      this.setState({
-        collapsed: true,
-      });
-    }
-  }
-}
-
 
 export default ExchangeArchives ;
